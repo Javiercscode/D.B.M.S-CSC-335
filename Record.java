85913e4a10e670c6eb216bb1bc6f594c61e3e618
Naruaki Alden Okashima <91356809+N-Alden-Okashima@users.noreply.github.com>
2024-10-26 20:32:34 -0700

    Record.java

--- Record.java
+++ Record.java
@@ -1,24 +1,33 @@
+import java.util.ArrayList;
 public class Record {
-    private String[] fields;
+    private ArrayList<Field> fields;
 
-    public String getField(int index) {
-        return fields[index];
+    public String getField(int ind) {
+        return (String)fields.get(ind);
     }
 
-    public void setField(int index, String n, String v) {
-        Field f = new Field(n, v);
-        fields[index] = f;
+    public void setField(Field old, Field n) {
+        int ind = fields.indexOf(old);
+        fields.set(ind, n);
     }
 
-    public boolean equals(Record r1, Record r2) {
-        if (r1.getField().equals(r2.getField())) {
-            return true;
-        } else {
-            return false;
+    public Record(Field f) {
+        fields.add(f);
+    }
+
+    public boolean isEqual(Record r1, Record r2) {
+        int num = 0;
+        while (num<fields.size()) {
+            if (r1.getField().equals(r2.getField())) {
+                num++;
+            } else {
+                return false;
+            }
         }
+        return true;
     }
 
-    public String toString(Field f) {
-        return ("Name: " + f.getName() + " Value: " + f.getValue());
+    public String toString() {
+        return ("Name: " + fields.get(0).getName() + " Value: " + fields.get(0).getValue());
     }
 }
