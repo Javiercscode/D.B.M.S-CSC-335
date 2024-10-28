c8269c0800ba86f8dea33c502a76d809743a1b85
Javiercscode <resendez@callutheran.edu>
2024-10-27 21:20:45 -0700

    Update DBMS.java

--- DBSM.java
+++ DBSM.java
@@ -1,51 +1,46 @@
 import java.io.File;
-import java.io.FileNotFoundException;
+public class DBMS extends Database{
+    File file = new File("Database.csv");
+    boolean File_connection;
+    
+    DBMS(){
 
-public class DBSM extends Database {
-    private boolean containsRecord; 
-    private File dbmsFile;
-    private boolean Connected;
+    }
+    public void Connect(){
+        if(file.exists()){
+            File_connection = true;
+            System.out.println("File does exist!");
+            System.out.println("Connection established");
+        }
+        else{
+            File_connection = false;
+            System.out.println("Could not connect to file:");
+            System.out.println("Reason:File could not be find");
+            System.exit(0);
+        }
 
-    public void connect(String filePath) {
-        dbmsFile = new File("C:\\Users\\XxGho\\Desktop\\CSC 335 java project\\DBMS 335\\Databasefile.csv"); 
-        try {
-            if (dbmsFile.exists()) {
-                Connected = true;
-                System.out.println("Connected to the database file.");
-            } else {
-                throw new FileNotFoundException("Database file not found.");
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    }
+    public void Disconnect(){
+        if(file.exists()){
+            System.out.println("File is found");
+            System.out.println("Sucessfully disconnected from file!");
+        }
+        else{
+            System.out.println("Could not disconnect from file:");
+            System.out.println("Reason:File could not be find");
+            //System.exit(0);
         }
     }
-
-    public void disconnect() {
-        if (dbmsFile != null && dbmsFile.exists()) {
-            dbmsFile = null;
-            System.out.println("Disconnected from the database.");
-        } else {
-            System.out.println("Can't disconnect; file doesn't exist.");
-        }
-    }
-
-    public void add(Record record) {
-        return super.addRecord(record);
-    }
-
-    public boolean contains(Record search) {
-        return super.containsRecord(search);
-    }
-    public static Contains(){
+    public void Contains(){
 
     }
-    public void update(Record record, Record modify){
-        super.updateRecord(record, modify);
+    public void Update(){
+
     }
-    public void insert(Record record){
-        super.insertRecord(record);
+    public void Insert(){
+
     }
-    public void delete(Record record){
-        super.deleteRecord(record);
+    public void Delete(){
+        
     }
 }
