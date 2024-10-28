3ff0e36782a735ed1ecdd6a59c0e59aa36ca6818
Javiercscode <resendez@callutheran.edu>
2024-10-27 21:21:23 -0700

    Update Field.java

--- Field.java
+++ Field.java
@@ -1,31 +1,59 @@
-public class Field {
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Field extends DBMS {
     private String name;
     private String value;
+    private FileWriter pw;
 
     public Field() {
-        name = "";
-        value = "";
+        this.name = "";
+        this.value = "";
     }
 
-    public Field(String n, String v) {
-        this.name = n;
-        this.String = s;
+    public Field(String name, String value) {
+        this.name = name;
+        this.value = value;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(string n) {
-        this.name = n;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getValue() {
         return value;
     }
 
-    public void setValue(String v) {
-        this.value = v;
+    public void setValue(String value) {
+        this.value = value;
     }
 
+    public void addRecord() {
+            try {
+                pw = new FileWriter("Database.csv", true);
+                pw.write(getName() + "," + getValue() + "\n");
+                System.out.println("Record added: " + getName() + " = " + getValue());
+            } catch (IOException e) {
+                System.out.println("An error occurred while writing to the file.");
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (pw != null) {
+                        pw.close();
+                    }
+                } catch (IOException e) {
+                    System.out.println("Failed to close the FileWriter.");
+                    e.printStackTrace();
+                }
+            }
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\n" + "Value: " + getValue();
+    }
 }
