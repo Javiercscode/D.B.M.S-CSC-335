eeb3e689761a83c129f4e0a2ff2ca7c16823f011
Javiercscode <resendez@callutheran.edu>
2024-10-27 21:21:03 -0700

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,71 +1,27 @@
 import java.util.ArrayList;
-import java.util.List;
-
 public class Database {
-    private List<Record> records;
+    ArrayList<String> Datalist = new ArrayList<>();
+    Database(){
 
-    public Database() {
-        records = new ArrayList<>();
+    }
+    public void DisplayRecord(){
+    
+    }
+    public void AddRecord(){
+
+    }
+    public void UpdateRecord(){
+
+    }
+    public void SelectRecord(){
+
+    }
+    public void DeleteRecord(){
+
+    }
+    public void ContainsRecord(){
+        
     }
 
-    public Record[] displayRecord() {
-        return records.toArray(new Record[0]);
-    }
-
-    public void addRecord(Record record) throws Exception {
-        if (containsRecord(record)) {
-            throw new Exception("Duplicate record exception: Record already exists.");
-        }
-        records.add(record);
-    }
-
-    public void updateRecord(Record search, Record modify) throws Exception {
-        boolean updated = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).equals(search)) {
-                records.set(i, modify);
-                updated = true;
-                break;
-            }
-        }
-        if (!updated) {
-            throw new Exception("Record not found exception: Unable to update, record not found.");
-        }
-    }
-
-    public Record[] selectRecord(Record search) throws Exception {
-        List<Record> selectedRecords = new ArrayList<>();
-        for (Record record : records) {
-            if (record.equals(search)) {
-                selectedRecords.add(record);
-            }
-        }
-        if (selectedRecords.isEmpty()) {
-            throw new Exception("Record not found exception: No matching records found.");
-        }
-        return selectedRecords.toArray(new Record[0]);
-    }
-
-    public void deleteRecord(Record search) throws Exception {
-        boolean deleted = false;
-        for (int i = 0; i < records.size(); i++) {
-            if (records.get(i).equals(search)) {
-                records.remove(i);
-                deleted = true;
-                break;
-            }
-        }
-        if (!deleted) {
-            throw new Exception("Record not found exception: Unable to delete, record not found.");
-        }
-    }
-
-    public boolean containsRecord(Record search) {
-        for (Record record : records) {
-            if (record.equals(search)) {
-                return true;
-            }
-        }
-        return false;
-    }
+    
 }
