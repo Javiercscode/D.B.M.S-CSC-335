f262cd20cf633274c8f478b80e9716822484a519
Javiercscode <resendez@callutheran.edu>
2024-10-27 22:37:27 -0700

    Update Database.java

--- Database.java
+++ Database.java
@@ -1,14 +1,37 @@
-import java.util.ArrayList;
+import java.io.*;
 public class Database {
-    ArrayList<String> Datalist = new ArrayList<>();
+    String file = "Database.csv";
     Database(){
 
     }
     public void DisplayRecord(){
-    
-    }
-    public void AddRecord(){
+        BufferedReader reader = null;
+        String line = "";
 
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            while((line = reader.readLine())!=null){
+                String[] row = line.split(",");
+
+                for(String index : row){
+                    System.out.printf("%-10s",index);
+                }
+                System.out.println();
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally{
+            try {
+                reader.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        }
+    
     }
     public void UpdateRecord(){
 
